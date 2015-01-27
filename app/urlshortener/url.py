b70fd80a2210b40d09a695ad8c5565187019b8b4
Felix Bade <felix@bade.fi>
2015-01-28 00:19:28 +0200

    Oh, it was only 2 months late

--- app/urlshortener/url.py
+++ app/urlshortener/url.py
@@ -11,7 +11,7 @@ def encodeURL(url):
     netloc = parsed.netloc.encode('idna')
     path = urllib.quote_plus(parsed.path.encode('utf-8'), '%/')
     params = parsed.params #urllib.quote_plus(parsed.params, '&=%')
-    query = urllib.quote_plus(parsed.query.encode('utf-8'), '&=%')
+    query = urllib.quote_plus(parsed.query.encode('utf-8'), '&=%/')
     fragment = urllib.quote_plus(parsed.fragment.encode('utf-8'), '&=%')
 
     return urlparse.urlunparse((scheme, netloc, path, params, query, fragment))
