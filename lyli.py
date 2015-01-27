467b9c6b32ea48aac1786148be53f3da59182592
Felix Bade <felix@bade.fi>
2015-01-28 00:04:58 +0200

    Add fork and log real ip

--- lyli.py
+++ lyli.py
@@ -1,3 +1,24 @@
 #!flask/bin/python
+import logging
+from os import fork
+
+import werkzeug.serving
+
 from app import app
-app.run(port=3004, debug=False)
+
+pid = fork()
+if pid > 0:
+    print('PID: %d' % pid)
+    exit(0)
+elif pid < 0:
+    print('Could not fork: %d' % pid)
+    exit(1)
+
+# we are behind a proxy. log the ip of the end-user, not the proxy.
+# this will also work without the proxy
+werkzeug.serving.WSGIRequestHandler.address_string = lambda self: self.headers.get('x-real-ip', self.client_address[0])
+
+# log to a file (access.log), not stderr
+logging.basicConfig(filename='access.log', level=logging.DEBUG, format='%(message)s')
+
+app.run(port=3004, debug=False, use_reloader=False)
