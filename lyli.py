73f49b5603802ccce3a9c4db0ee0b2eaa4bf0e7f
Felix Bade <felix@bade.fi>
2015-05-10 12:52:52 +0300

    Update startup script (lyli.py)

--- lyli.py
+++ lyli.py
@@ -1,18 +1,10 @@
 #!flask/bin/python
 import logging
-from os import fork
 
 import werkzeug.serving
 
 from app import app
-
-pid = fork()
-if pid > 0:
-    print('PID: %d' % pid)
-    exit(0)
-elif pid < 0:
-    print('Could not fork: %d' % pid)
-    exit(1)
+import config
 
 # we are behind a proxy. log the ip of the end-user, not the proxy.
 # this will also work without the proxy
@@ -21,5 +13,7 @@ werkzeug.serving.WSGIRequestHandler.address_string = lambda self: self.headers.g
 # log to a file (access.log), not stderr
 logging.basicConfig(filename='access.log', level=logging.DEBUG, format='%(message)s')
 
-app.run(port=3004, debug=False, use_reloader=False)
-#app.run(port=3003, debug=True, use_reloader=True)
+if config.debug:
+    app.run(port=3003, debug=True, use_reloader=True)
+else:
+    app.run(port=3004, debug=False, use_reloader=False)
